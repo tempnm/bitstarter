f42f4c4ba1c9eed38e5958a756f45c9378235e39
Rich Hall <richjdhall@virginmedia.com>
2013-07-19 11:00:53 +0000

    Start again with file.

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+/*
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs = require('fs');
+
+  var buf = new Buffer(fs.readFileSync('index.html', 'utf8'));
+
+  response.send('Temp \n');
+
+});
+*/
+app.get('/', function(request, response) {
+  response.send('Hello World 4!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+})
