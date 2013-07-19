f181df85dd8b81af870c6c1780d581bfacfa5cbe
Rich Hall <richjdhall@virginmedia.com>
2013-07-19 11:38:00 +0000

    step 4 - Part 1 complete?

--- web.js
+++ web.js
@@ -7,17 +7,11 @@ app.get('/', function(request, response) {
 
   var buf = new Buffer(fs.readFileSync('index.html', 'utf8'));
 
-//  var buf = new Buffer(256);
-
-  response.send('Temp ' + buf.toString()  + ' bits  \n');
+  response.send(buf.toString());
 
 });
 
-/*
-app.get('/', function(request, response) {
-  response.send('Hello World 4!');
-});
-*/
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
